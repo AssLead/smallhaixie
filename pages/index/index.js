8339ef3d6974e1cb5578c8d89adc5a77f3a05b47
= <=>
2018-02-24 18:41:00 +0800

    ..

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,22 +3,19 @@
 const app = getApp()
 Page({
   data: {
-    width: app.systemInfo.windowWidth,
-    height: app.systemInfo.windowHeight,
-    banner: ['http://i.dxlfile.com/adm/material/2016_12_12/20161212135600242250.jpg',
+
+    banner:
+     ['http://i.dxlfile.com/adm/material/2016_12_12/20161212135600242250.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_04/2017010411165785666.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_04/20170104140739205869.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_16/20170116171332214897.jpg'],
     functions: [
      
     ],
-
   },
-
   onLoad: function () {
 
   },
-
   fucClick(event) {
     const id = event.currentTarget.dataset.id;
     console.log(id);
