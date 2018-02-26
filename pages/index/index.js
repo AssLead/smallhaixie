cdcb40c90d0906bcb889d2db972964dfa7790b0d
= <=>
2018-02-26 18:32:05 +0800

    2018-02-26

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,15 +3,11 @@
 const app = getApp()
 Page({
   data: {
-
     banner:
      ['http://i.dxlfile.com/adm/material/2016_12_12/20161212135600242250.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_04/2017010411165785666.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_04/20170104140739205869.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_16/20170116171332214897.jpg'],
-    functions: [
-     
-    ],
   },
   onLoad: function () {
 
@@ -29,5 +25,6 @@ Page({
       // url: '../goods/goods',
     })
   }
+  
 
 })
