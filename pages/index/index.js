65d337efd975acfa0385d2b3b809d01336f35853
= <=>
2018-03-06 19:08:11 +0800

    .

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,101 @@
-//index.js
-//获取应用实例
-const app = getApp()
+var WxParse = require('../../wxParse/wxParse.js');
+var config = require('../../config.js');
 Page({
-  data: {
-    banner:
-     ['http://i.dxlfile.com/adm/material/2016_12_12/20161212135600242250.jpg',
-      'http://i.dxlfile.com/adm/material/2017_01_04/2017010411165785666.jpg',
-      'http://i.dxlfile.com/adm/material/2017_01_04/20170104140739205869.jpg',
-      'http://i.dxlfile.com/adm/material/2017_01_16/20170116171332214897.jpg'],
+
+  /**
+   * 页面的初始数据
+   */
+ 
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.showToast({
+      title: "加载中...",
+      icon: "loading",
+      duration: 10000
+    });
+    wx.request({
+      url: config.scenicDetails,
+      data: {
+        // "pkScenic": 29
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        wx.hideToast();
+        var data = res.data;
+        // console.log(res.data.data);
+        if (data.status == "SUCCESS") {
+          // var profile = data.data.scProfile;
+          var logo = config.host + data.data.ad_code;
+
+          that.setData({
+            // profile: profile && (profile.slice(0, 40) + "..."),
+            logo: logo,
+   
+            data: {
+              imgUrls: [
+                config.host + data.data.ad_code
+              ]
+            },
+          });
+        }
+
+      }
+    });
   },
-  onLoad: function () {
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
-  fucClick(event) {
-    const id = event.currentTarget.dataset.id;
-    console.log(id);
-    wx.navigateTo({
-      // url: '../storelist/storelist',
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
   },
-  goodDetail(event) {
-    wx.navigateTo({
-      // url: '../goods/goods',
-    })
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
-  
-
 })
